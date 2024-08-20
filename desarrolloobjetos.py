dc8193158d4d0e70ef212791f81e447a20238db1
el papy <165804945+benjaaam0@users.noreply.github.com>
2024-08-20 09:16:25 -0400

    Update desarrolloobjetos.py

--- desarrolloobjetos.py
+++ desarrolloobjetos.py
@@ -12,3 +12,24 @@ peso
 edad
 estatura
 nombre
+
+caracteristicas o atributos son ejemplo telefono son : pantalla,almacenamiento,bateria
+y las acciones son las que se pueden realizar ejemplo: llamar,cargar,chatear,navegar
+   ejmplo
+clase o molde: pizza
+
+atri o caract : tamaño,masa,ingredientes,precio
+
+acciones : vender() comer() calentar()
+
+Java : poner si es private o public despues de un int y num
+
+encapculamiento : se define el tipo de acceso que puede ser private,public y protected
+
+polimorfismo: misma accion de algo pero hecho de distintas formas como hablar,tu puedes hablar en español,frances,ingles,frances
+
+abtraccion: dependiendo del contexto debemos tomar la informacion importante de una intidad u  objeto y desechar lo inutil
+
+//Comentario
+        System.out.print("hello mundoo");
+        sout + tab
